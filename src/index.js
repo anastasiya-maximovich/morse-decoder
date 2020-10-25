b86a17205a69194b1b2ddba07d70991d1d86640c
anastasiya023 <anastasiya025.am@gmail.com>
2020-10-25 21:57:26 +0300

    feat: add .js

--- src/index.js
+++ src/index.js
@@ -36,9 +36,37 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
 function decode(expr) {
-    // write your solution here
+    let arr = [], arrNew = [], n = 0, str = '', strNew = '';
+  
+    for (let i = 0; i < expr.length / 10; i++) {
+    arr[i] = expr.substr(n, 10);
+    n +=10;
+        }
+
+arr.forEach(function(item, i) { 
+  strNew = item.toString();
+  str = strNew.replace(/11/g, '-');
+  str1 = str.replace(/10/g, '.');
+  str2 = str1.replace(/0/g, '');
+  arr[i] = str2;
+  if (item == '**********') {
+  arr[i] = " ";
+  }
+});
+
+let arrMorseKeys = Object.keys(MORSE_TABLE);
+let arrMorseValues = Object.values(MORSE_TABLE); 
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] == " ") {
+    arr[i] = " ";
+  } else {
+  arr[i] = arrMorseValues[arrMorseKeys.indexOf(arr[i])];
+}}
+
+let answer = arr.join('').toString();
+return answer;
 }
 
 module.exports = {
